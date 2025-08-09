84ea9c287c83a12133edd5ba15f0dea8fda0d363
tazeek <tazeek.rakib@gmail.com>
2025-08-09 17:28:36 +0800

    Lets start

---
+++ same_page/scraper.js
@@ -0,0 +1,19 @@
+
+
+// Paste into console within the web page
+
+function scrapeVernaBanana() {
+    //Link: https://www.vernabanana.com/recipes
+
+    // Get the collection squares
+
+    // Go one by one
+    
+    // Extract the title
+
+    // Extract the web link
+
+    // Sort by alphabetical order
+
+    // Copy to clipboard
+}
