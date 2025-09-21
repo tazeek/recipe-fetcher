d76f0e2dda3a3d079073d1b87d894c609c5be0bb
tazeek <tazeek.rakib@gmail.com>
2025-09-21 11:35:39 +0800

    Single file created

---
+++ different_pages/single_file_combo.py
@@ -0,0 +1,18 @@
+import os
+
+def collect_data():
+    ...
+
+def tabulate_data():
+    ...
+
+def store_data():
+    ...
+
+# Load all the files
+combined_files = collect_data()
+
+# Tabulated data
+tabulated_data = tabulate_data()
+
+store_data()
